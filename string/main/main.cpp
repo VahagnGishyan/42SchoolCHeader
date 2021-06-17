b52ed45b898279c85b0e56294b28b099f1788dae
Vahagn Gishyan <vahagn.gishyan.a@gmail.com>
2021-06-17 21:05:12 +0400

    first commit

---
+++ string/main/main.cpp
@@ -0,0 +1,13 @@
+#include "../header/header.h"
+#include "../header/test.h"
+#include "../header/cpp_header.h"
+
+int main()
+{
+    std::cout << "Start main()\n";
+
+    Test_CSString::All();
+
+    std::cout << "Exit main()\n";
+    return 0;
+}
