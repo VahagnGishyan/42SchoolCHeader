5f7caa63fe88362e9d4af732932de13b3606c7da
Vahagn Gishyan <vahagn.gishyan.a@gmail.com>
2021-06-19 11:34:49 +0400

    42SchoolCHeader

---
+++ string/header/42SchoolCHeader.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "c_header.h"
+#include "../libft/Libft.h"
+#include "../c_sstring/c_sstring.h"
+
+
+typedef unsigned short int ushint;
+typedef unsigned short int ushort;
+
+typedef          short int shint;
+typedef unsigned       int uint;
+
+typedef unsigned long  int ulong;
+typedef unsigned long  int ulint;
