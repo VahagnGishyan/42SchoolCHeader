b6ac946ff2becfaf193b4186da9887dfc114324d
Vahagn Gishyan <vahagn.gishyan.a@gmail.com>
2021-06-22 14:59:32 +0400

    test c_sstring

---
+++ string/test/test_c_sstring/test_string.cpp
@@ -0,0 +1,20 @@
+#include "test_string.h"
+
+void Test_CSString::All()
+{
+    std::cout << "Test_CSString" << std::endl;
+    Initialization();
+    ArraySize();
+    IndexOperators();
+    PushPopBack();
+    CopyData();
+    Concatenation();
+    Compare();
+    Search();
+    IsCheck();
+    ToUpper();
+    ToLower();
+    ConvertToInt();
+    ConvertToCharArray();
+}
+
