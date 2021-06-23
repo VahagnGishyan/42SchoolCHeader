50a1f42143cfe4f842f8f85f6ae5f343a446509c
Vahagn Gishyan <vahagn.gishyan.a@gmail.com>
2021-06-24 00:44:48 +0400

    libft

--- string/libft/LIbft.c
+++ string/libft/LIbft.c
@@ -17,8 +17,8 @@ bool is_null(const char* ptr)
     return (ptr == NULL);
 }
 
-bool get_dynamic_memory(char* ptr, int size)
+bool get_dynamic_memory(char** ptr, int size)
 {
-    ptr = (char*)malloc(size);
+    ptr[0] = (char*)malloc(size);
     return (ptr == NULL);
 }
