64d033c9328974687f10aaab02c6ae7c5f34a06d
Vahagn Gishyan <vahagn.gishyan.a@gmail.com>
2021-06-24 00:45:27 +0400

    main

--- string/main/main.cpp
+++ string/main/main.cpp
@@ -1,4 +1,4 @@
-#include "../header/header.h"
+
 #include "../header/test.h"
 #include "../header/cpp_header.h"
 
@@ -7,6 +7,7 @@ int main()
     std::cout << "Start main()\n";
 
     Test_CSString::All();
+
 
     std::cout << "Exit main()\n";
     return 0;
