e04c678d00e7af24bc213cd6706b9af74ad43cb0
Vahagn Gishyan <vahagn.gishyan.a@gmail.com>
2021-06-27 11:53:33 +0400

    convert to int

--- string/test/test_c_sstring/Test_CSString_ConvertToInt.cpp
+++ string/test/test_c_sstring/Test_CSString_ConvertToInt.cpp
@@ -1,4 +1,4 @@
-#include "test_string.h"
+#include "Test_CSString.h"
 
 void Test_CSString::ConvertToInt()
 {
@@ -6,11 +6,35 @@ void Test_CSString::ConvertToInt()
 
     bool result = true;
 
+    c_sstring str0;
+    c_sstring str1;
+    c_sstring str2;
+    c_sstring str3;
+    c_sstring str4A;
+    c_sstring str4B;
+    c_sstring str5A;
+    c_sstring str5B;
+    c_sstring str6A;
+    c_sstring str6B;
+    c_sstring str7A;
+    c_sstring str7B;
+
+    c_sstring_constructor(&str0);
+    c_sstring_constructor(&str1);
+    c_sstring_constructor(&str2);
+    c_sstring_constructor(&str3);
+    c_sstring_constructor(&str4A);
+    c_sstring_constructor(&str4B);
+    c_sstring_constructor(&str5A);
+    c_sstring_constructor(&str5B);
+    c_sstring_constructor(&str6A);
+    c_sstring_constructor(&str6B);
+    c_sstring_constructor(&str7A);
+    c_sstring_constructor(&str7B);
+
     /*Check 0*/
     char arr0[1] = { '5' };
-    c_sstring str0;
-    c_sstring_initaliaztion(&str0);
-    str0.constructor(&str0, arr0, 1, 1);
+    str0.setdata(&str0, arr0, 1, 1);
     int x0; 
     str0.convert_to_int(&str0, &x0);
     if (x0 != 5)
@@ -27,9 +51,7 @@ void Test_CSString::ConvertToInt()
 
     /*Check 1*/
     char arr1[128] = { "5" };
-    c_sstring str1;
-    c_sstring_initaliaztion(&str1);
-    str1.constructor(&str1, arr1, std::strlen(arr1), 128);
+    str1.setdata(&str1, arr1, std::strlen(arr1), 128);
     int x1;
     str1.convert_to_int(&str1, &x1);
     if (x1 != 5)
@@ -45,9 +67,7 @@ void Test_CSString::ConvertToInt()
 
     /*Check 2*/
     char arr2[228] = { "51286" };
-    c_sstring str2;
-    c_sstring_initaliaztion(&str2);
-    str2.constructor(&str2, arr2, std::strlen(arr2), 228);
+    str2.setdata(&str2, arr2, std::strlen(arr2), 228);
     int x2;
     str2.convert_to_int(&str2, &x2);
     if (x2 != 51286)
@@ -63,9 +83,7 @@ void Test_CSString::ConvertToInt()
 
     /*Check 3*/
     char arr3[338] = { "51386A" };
-    c_sstring str3;
-    c_sstring_initaliaztion(&str3);
-    str3.constructor(&str3, arr3, std::strlen(arr3), 338);
+    str3.setdata(&str3, arr3, std::strlen(arr3), 338);
     int x3;
     str3.convert_to_int(&str3, &x3);
     if (x3 != std::stoi(arr3))
@@ -79,10 +97,10 @@ void Test_CSString::ConvertToInt()
 	std::cout << "\tstr3.convert_to_int(...) = " << x3 << "\t:: must be " << 5 << std::endl;
     }
 
-    str0.destructor(&str0);
-    str1.destructor(&str1);
-    str2.destructor(&str2);
-    str3.destructor(&str3);
+    c_sstring_destructor(&str0);
+    c_sstring_destructor(&str1);
+    c_sstring_destructor(&str2);
+    c_sstring_destructor(&str3);
 
     if (result)
 	std::cout << "TRUE" << std::endl;
