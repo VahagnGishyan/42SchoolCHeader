07460a9937d68d50a8638e82943d1647abcda06e
Vahagn Gishyan <vahagn.gishyan.a@gmail.com>
2021-06-27 11:49:12 +0400

    Array size

--- string/test/test_c_sstring/Test_CSString_ArraySize.cpp
+++ string/test/test_c_sstring/Test_CSString_ArraySize.cpp
@@ -1,4 +1,4 @@
-#include "test_string.h"
+#include "Test_CSString.h"
 
 void Test_CSString::ArraySize()
 {
@@ -6,11 +6,20 @@ void Test_CSString::ArraySize()
 
     bool result = true;
 
+    c_sstring str0;
+    c_sstring str1;
+    c_sstring str2;
+    c_sstring str3;
+
+    c_sstring_constructor(&str0);
+    c_sstring_constructor(&str1);
+    c_sstring_constructor(&str2);
+    c_sstring_constructor(&str3);
+
+
     /*Check 0*/
     char arr0[1];
-    c_sstring str0;
-    c_sstring_initaliaztion(&str0);
-    str0.constructor(&str0, arr0, 0, 1);
+    str0.setdata(&str0, arr0, 0, 1);
     if (str0.size(&str0) != 0 || str0.capacity(&str0) != 1 || !str0.is_empty(&str0))
     {
 	result = false;
@@ -23,9 +32,7 @@ void Test_CSString::ArraySize()
 
     /*Check 1*/
     char arr1[1]{ 'a' };
-    c_sstring str1;
-    c_sstring_initaliaztion(&str1);
-    str1.constructor(&str1, arr1, 0, 1);
+    str1.setdata(&str1, arr1, 0, 1);
     str1.clear(&str1);
     if (str1.size(&str1) != 0 || str1.capacity(&str1) != 1 || !str1.is_empty(&str1))
     {
@@ -42,9 +49,7 @@ void Test_CSString::ArraySize()
 
     /*Check 2*/
     char arr2[1]{ 'a' };
-    c_sstring str2;
-    c_sstring_initaliaztion(&str2);
-    str2.constructor(&str2, arr2, 1, 128);
+    str2.setdata(&str2, arr2, 1, 128);
     str2.clear(&str2);
     if (str2.size(&str2) != 0 || str2.capacity(&str2) != 128 || !str2.is_empty(&str2))
     {
@@ -61,9 +66,7 @@ void Test_CSString::ArraySize()
 
     /*Check 3*/
     char arr3[128]{ "abcde" };
-    c_sstring str3;
-    c_sstring_initaliaztion(&str3);
-    str3.constructor(&str3, arr3, 5, 128);
+    str3.setdata(&str3, arr3, 5, 128);
     str3.clear(&str3);
     if (str3.size(&str3) != 0 || str3.capacity(&str3) != 128 || !str3.is_empty(&str3))
     {
@@ -78,10 +81,10 @@ void Test_CSString::ArraySize()
 	std::cout << "\t!str.is_empty() = " << !str3.is_empty(&str3) << "\t:: must be false" << std::endl;
     }
 
-    str0.destructor(&str0);
-    str1.destructor(&str1);
-    str2.destructor(&str2);
-    str3.destructor(&str3);
+    c_sstring_destructor(&str0);
+    c_sstring_destructor(&str1);
+    c_sstring_destructor(&str2);
+    c_sstring_destructor(&str3);
 
     if (result)
 	std::cout << "TRUE" << std::endl;
