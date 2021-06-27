db998477e4a76de498328f08ee9bdd293a9bc7e8
Vahagn Gishyan <vahagn.gishyan.a@gmail.com>
2021-06-27 11:51:40 +0400

    test compare data

--- string/test/test_c_sstring/Test_CSString_CompareData.cpp
+++ string/test/test_c_sstring/Test_CSString_CompareData.cpp
@@ -1,4 +1,4 @@
-#include "test_string.h"
+#include "Test_CSString.h"
 
 void Test_CSString::Compare()
 {
@@ -6,12 +6,36 @@ void Test_CSString::Compare()
 
     bool result = true;
 
+    c_sstring str0;
+    c_sstring str1;
+    c_sstring str2;
+    c_sstring str3;
+    c_sstring str4A;
+    c_sstring str4B;
+    c_sstring str5A;
+    c_sstring str5B;
+    c_sstring str6A;
+    c_sstring str6B;
+    c_sstring str7A;
+    c_sstring str7B;
+
+    c_sstring_constructor(&str0);
+    c_sstring_constructor(&str1);
+    c_sstring_constructor(&str2);
+    c_sstring_constructor(&str3);
+    c_sstring_constructor(&str4A);
+    c_sstring_constructor(&str4B);
+    c_sstring_constructor(&str5A);
+    c_sstring_constructor(&str5B);
+    c_sstring_constructor(&str6A);
+    c_sstring_constructor(&str6B);
+    c_sstring_constructor(&str7A);
+    c_sstring_constructor(&str7B);
+
     /*Check 0*/
     char arr0A[1] = { 'V' };
     char arr0B[1] = { 'V' };
-    c_sstring str0;
-    c_sstring_initaliaztion(&str0);
-    str0.constructor(&str0,arr0A, 1, 128);
+    str0.setdata(&str0,arr0A, 1, 128);
     if (str0.compare_chararray(&str0,arr0A, 1) || str0.compare_chararray(&str0, arr0B, 1))
     {
 	if (result)
@@ -29,9 +53,7 @@ void Test_CSString::Compare()
     /*Check 1*/
     char arr1A[1] = { 'V' };
     char arr1B[1] = { 'A' };
-    c_sstring str1;
-    c_sstring_initaliaztion(&str1);
-    str1.constructor(&str1, arr1A, 1, 128);
+    str1.setdata(&str1, arr1A, 1, 128);
     if (str1.compare_chararray(&str1, arr1A, 1) || !str1.compare_chararray(&str1, arr1B, 1))
     {
 	if (result)
@@ -49,9 +71,7 @@ void Test_CSString::Compare()
     /*Check 2*/
     char arr2A[128] = { "VAHAGN" };
     char arr2B[128] = { "VAHAGN" };
-    c_sstring str2;
-    c_sstring_initaliaztion(&str2);
-    str2.constructor(&str2, arr2A, 6, 228);
+    str2.setdata(&str2, arr2A, 6, 228);
     if (str2.compare_chararray(&str2, arr2A, 6) || str2.compare_chararray(&str2, arr2B, 6))
     {
 	if (result)
@@ -69,9 +89,7 @@ void Test_CSString::Compare()
     /*Check 3*/
     char arr3A[128] = { "VAHAGN" };
     char arr3B[128] = { "VAHAGNA" };
-    c_sstring str3;
-    c_sstring_initaliaztion(&str3);
-    str3.constructor(&str3, arr3A, 6, 338);
+    str3.setdata(&str3, arr3A, 6, 338);
     if (str3.compare_chararray(&str3, arr3A, 6) || !str3.compare_chararray(&str3, arr3B, 7))
     {
 	if (result)
@@ -89,12 +107,8 @@ void Test_CSString::Compare()
     /*Check 4*/
     char arr4A[4] = { 'V' };
     char arr4B[4] = { 'V' };
-    c_sstring str4A;
-    c_sstring_initaliaztion(&str4A);
-    c_sstring str4B;
-    c_sstring_initaliaztion(&str4B);
-    str4A.constructor(&str4A, arr4A, 1, 428);
-    str4B.constructor(&str4B, arr4B, 1, 428);
+    str4A.setdata(&str4A, arr4A, 1, 428);
+    str4B.setdata(&str4B, arr4B, 1, 428);
     if (str4A.compare_c_sstring(&str4A, &str4B) || str4B.compare_c_sstring(&str4B, &str4A))
     {
 	if (result)
@@ -112,12 +126,8 @@ void Test_CSString::Compare()
     /*Check 5*/
     char arr5A[1] = { 'V' };
     char arr5B[1] = { 'A' };
-    c_sstring str5A;
-    c_sstring_initaliaztion(&str5A);
-    c_sstring str5B;
-    c_sstring_initaliaztion(&str5B);
-    str5A.constructor(&str5A,arr5A, 1, 1);
-    str5B.constructor(&str5B, arr5B, 1, 1);
+    str5A.setdata(&str5A,arr5A, 1, 1);
+    str5B.setdata(&str5B, arr5B, 1, 1);
     if (!str5A.compare_c_sstring(&str5A, &str5B) || !str5B.compare_c_sstring(&str5B, &str5A))
     {
 	if (result)
@@ -135,12 +145,8 @@ void Test_CSString::Compare()
     /*Check 6*/
     char arr6A[128] = { "VAHAGN" };
     char arr6B[128] = { "VAHAGN" };
-    c_sstring str6A;
-    c_sstring_initaliaztion(&str6A);
-    c_sstring str6B;
-    c_sstring_initaliaztion(&str6B);
-    str6A.constructor(&str6A, arr6A, 6, 128);
-    str6B.constructor(&str6B, arr6B, 6, 128);
+    str6A.setdata(&str6A, arr6A, 6, 128);
+    str6B.setdata(&str6B, arr6B, 6, 128);
     if (str6A.compare_c_sstring(&str6A, &str6B) || str6B.compare_c_sstring(&str6B, &str6A))
     {
 	if (result)
@@ -158,12 +164,8 @@ void Test_CSString::Compare()
     /*Check 7*/
     char arr7A[128] = { "VAHAGN" };
     char arr7B[128] = { "VAHAGNA" };
-    c_sstring str7A;
-    c_sstring_initaliaztion(&str7A);
-    c_sstring str7B;
-    c_sstring_initaliaztion(&str7B);
-    str7A.constructor(&str7A, arr7A, 6, 128);
-    str7B.constructor(&str7B, arr7B, 7, 128);
+    str7A.setdata(&str7A, arr7A, 6, 128);
+    str7B.setdata(&str7B, arr7B, 7, 128);
     if (!str7A.compare_c_sstring(&str7A, &str7B) || !str7B.compare_c_sstring(&str7B, &str7A))
     {
 	if (result)
@@ -178,18 +180,18 @@ void Test_CSString::Compare()
 	    "\t:: must be 0" << std::endl;
     }
 
-    str0.destructor(&str0);
-    str1.destructor(&str1);
-    str2.destructor(&str2);
-    str3.destructor(&str3);
-    str4A.destructor(&str4A);
-    str4B.destructor(&str4B);
-    str5A.destructor(&str5A);
-    str5B.destructor(&str5B);
-    str6A.destructor(&str6A);
-    str6B.destructor(&str6B);
-    str7A.destructor(&str7A);
-    str7B.destructor(&str7B);
+    c_sstring_destructor(&str0);
+    c_sstring_destructor(&str1);
+    c_sstring_destructor(&str2);
+    c_sstring_destructor(&str3);
+    c_sstring_destructor(&str4A);
+    c_sstring_destructor(&str4B);
+    c_sstring_destructor(&str5A);
+    c_sstring_destructor(&str5B);
+    c_sstring_destructor(&str6A);
+    c_sstring_destructor(&str6B);
+    c_sstring_destructor(&str7A);
+    c_sstring_destructor(&str7B);
 
     if (result)
 	std::cout << "TRUE" << std::endl;
