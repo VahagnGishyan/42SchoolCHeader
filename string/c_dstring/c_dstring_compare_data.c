698bbf775c7332bc361f99dec5b9290b4e3fbb37
Vahagn Gishyan <vahagn.gishyan.a@gmail.com>
2021-06-30 03:00:46 +0400

    Compare data

---
+++ string/c_dstring/c_dstring_compare_data.c
@@ -0,0 +1,16 @@
+#include "c_dstring.h"
+
+void __c_dstring_constructor_compare_data(t_c_dstring* str)
+{
+    str->compare_chararray = c_dstring_compare_chararray;
+    str->compare_c_dstring = c_dstring_compare_c_dstring;
+}
+
+int c_dstring_compare_chararray(t_c_dstring* str, char* arr, int size)
+{
+    return(ft_strncmp(str->m_array, arr, size));
+}
+int c_dstring_compare_c_dstring(t_c_dstring* str_main, t_c_dstring* str_other)
+{
+    return(ft_strncmp(str_main->m_array, str_other->m_array, max(str_main->m_size, str_other->m_size)));
+}
