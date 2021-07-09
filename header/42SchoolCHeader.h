ec6f63598ace37fb6be1da333e3de67454756649
Vahagn Gishyan <vahagn.gishyan.a@gmail.com>
2021-07-09 17:31:22 +0400

    42ShchoolCHeader

---
+++ header/42SchoolCHeader.h
@@ -0,0 +1,27 @@
+#pragma once
+
+//  C standard library
+#include "../header/c_header.h"
+
+//  C Libft
+#include "../libft/Libft.h"
+
+//  C is easy
+#include "c_is_easy.h"
+
+//  C main containers
+#include "../c_string/c_sstring/c_sstring.h"
+#include "../c_string/c_dstring/c_dstring.h"
+#include "../c_string/c_string/c_string.h"
+
+
+
+
+typedef unsigned short int ushint;
+typedef unsigned short int ushort;
+
+typedef          short int shint;
+typedef unsigned       int uint;
+
+typedef unsigned long  int ulong;
+typedef unsigned long  int ulint;
