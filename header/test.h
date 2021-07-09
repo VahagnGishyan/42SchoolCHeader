8a5951e90aaf1ac056f36fcdddbde4bb6247171b
Vahagn Gishyan <vahagn.gishyan.a@gmail.com>
2021-07-09 17:33:31 +0400

    Test

---
+++ header/test.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "cpp_header.h"
+
+//  Test C string 
+#include "../test/test_c_sstring/Test_CSString.h"
+#include "../test/test_c_dstring/Test_CDString.h"
+#include "../test/test_c_string/Test_CString.h"
+
+//  Test C array int
+#include "../test/test_c_sarray_int/Test_CSArrayInt.h"
+#include "../test/test_c_darray_int/Test_CDArrayInt.h"
+#include "../test/test_c_vector_int/Test_CVectorInt.h"
