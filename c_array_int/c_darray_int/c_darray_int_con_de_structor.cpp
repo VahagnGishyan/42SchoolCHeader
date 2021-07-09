343d818a6d2e29cece6485a06315bff3896855f7
Vahagn Gishyan <vahagn.gishyan.a@gmail.com>
2021-07-09 17:37:40 +0400

    Constructor destructor

---
+++ c_array_int/c_darray_int/c_darray_int_con_de_structor.cpp
@@ -0,0 +1,36 @@
+#include "c_darray_int.h"
+
+//void c_string_constructor(t_c_string* str)
+//{
+//    str->m_size = 0;
+//    str->m_static_array = 0;
+//    str->m_static_capacity = 0;
+//    str->m_dynamic_array = 0;
+//    str->m_dynamic_capacity = 0;
+//
+//    __c_string_constructor_array_size(str);
+//    __c_string_constructor_compare_data(str);
+//    __c_string_constructor_concatenation(str);
+//    __c_string_constructor_convert_to(str);
+//    __c_string_constructor_copy_data(str);
+//    __c_string_constructor_get_free_dymemory(str);
+//    __c_string_constructor_index_operators(str);
+//    __c_string_constructor_is_check(str);
+//    __c_string_constructor_is_check_memory(str);
+//    __c_string_constructor_push_pop(str);
+//    __c_string_constructor_search(str);
+//    __c_string_constructor_to_upper_lower(str);
+//    __c_string_constructor_setdata(str);
+//}
+
+void c_darray_int_constructor(t_c_darray_int* str)
+{
+    str->m_array = 0;
+    str->m_size = 0;
+    str->m_capacity = 0;
+}
+
+void c_darray_int_destructor(t_c_darray_int* str)
+{
+    str->clear(str);
+}
