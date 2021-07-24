131927e19044b4352e2d0bcbfb7b0d5610e05b94
Vahagn Gishyan <vahagn.gishyan.a@gmail.com>
2021-07-25 00:46:09 +0400

    Resize clear

---
+++ c_array_int/c_darray_int/c_darray_int_resize_clear.c
@@ -0,0 +1,42 @@
+#include "c_darray_int.h"
+
+void __c_darray_int_constructor_resize_clear_dymemory(t_c_darray_int* str)
+{
+    str->resize = c_darray_int_resize;
+    str->clear = c_darray_int_clear;
+}
+
+bool c_darray_int_resize(t_c_darray_int* str, int length)
+{
+    int* newarr;
+    int  newlength;
+    int size;
+
+    if (length < 0)
+    {
+	c_darray_int_clear(str);
+	return (false);
+    }
+    if (str->m_capacity >= length)
+	return true;
+    newlength = length * 1.3;
+    if ((newarr = (int*)malloc(newlength * sizeof(int))) == NULL)
+	return NULL;
+    intarray_copy_intarray(newarr, str->m_array, str->m_size);
+    size = str->m_size;
+    c_darray_int_clear(str);
+    str->m_array = newarr;
+    str->m_size = size;
+    str->m_capacity = newlength;
+    return true;
+}
+
+void c_darray_int_clear(t_c_darray_int* str)
+{
+    if (str->m_array == NULL)
+	return;
+    free(str->m_array);
+    str->m_array = NULL;
+    str->m_size = 0;
+    str->m_capacity = 0;
+}
