cb6722b12e803d4aa0fd62a2a5a4d1edf7c304f8
Vahagn Gishyan <vahagn.gishyan.a@gmail.com>
2021-07-26 00:21:04 +0400

    Update CSArray Int

--- test/test_c_sarray_int/Test_CSArrayInt.h
+++ test/test_c_sarray_int/Test_CSArrayInt.h
@@ -1,18 +1,17 @@
 #pragma once
 
 #include "../../header/cpp_header.h"
-#include "../../c_array_int/c_sarray_int/c_sarray_int.h"
 
-//#ifdef __cplusplus
-//extern "C" 
-//{
-//#endif
-//
-//#include "../../header/42SchoolCHeader.h"
-//
-//#ifdef __cplusplus
-//}
-//#endif
+#ifdef __cplusplus
+extern "C" 
+{
+#endif
+
+#include "../../header/42SchoolCHeader.h"
+
+#ifdef __cplusplus
+}
+#endif
 
 namespace Test_CSArrayInt
 {
