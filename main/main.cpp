d9ce1e8e9d3d932d778f37a5efb580a3b96964d7
Vahagn Gishyan <vahagn.gishyan.a@gmail.com>
2021-07-27 10:09:39 +0400

    Main

--- main/main.cpp
+++ main/main.cpp
@@ -6,12 +6,14 @@ int main()
 {
     std::cout << "Start main()\n";
 
+
     //Test_CSString::All();
     //Test_CDString::All();
     //Test_CString::All();
-    Test_CSArrayInt::All();
-    Test_CDArrayInt::All();
-    Test_CVectorInt::All();
+    //Test_CSArrayInt::All();
+    //Test_CDArrayInt::All();
+    //Test_CVectorInt::All();
+    Test_CSArrayString::All();
 
     std::cout << "Exit main()\n";
     return 0;
