d6592c8bcc50d7d8e7eb777b24a3621742df602c
Vahagn Gishyan <vahagn.gishyan.a@gmail.com>
2021-07-31 10:05:51 +0400

    Convert to

---
+++ c_array_string/c_sarray_string/c_sarray_string_convert_to.cpp
@@ -0,0 +1,10 @@
+#include "c_sarray_string.h"
+
+void  c_sarray_string::convert_to_string_array(t_c_sarray_string* data, t_c_string* arrstring)
+{
+    int index;
+
+    index = -1;
+    while (++index < data->size(data))
+	arrstring[index] = *data->at(data, index);
+}
