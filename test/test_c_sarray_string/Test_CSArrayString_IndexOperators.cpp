e88889dd0fe8aa664b3e741fd16a9284d4333f95
Vahagn Gishyan <vahagn.gishyan.a@gmail.com>
2021-08-11 15:40:19 +0400

    Index operators

--- test/test_c_sarray_string/Test_CSArrayString_IndexOperators.cpp
+++ test/test_c_sarray_string/Test_CSArrayString_IndexOperators.cpp
@@ -12,7 +12,7 @@ void Test_CSArrayString::IndexOperators()
     constexpr int length = 8;
     c_string arrsorc[length];
     c_string_array_constructor(arrsorc, length);
-    c_string_copy_chararray(&arrsorc[0], "", 0);
+    c_string_copy_chararray(&arrsorc[0], "", 1);
     c_string_copy_chararray(&arrsorc[1], "Vahagn", 6);
     c_string_copy_chararray(&arrsorc[2], " ", 1);
     c_string_copy_chararray(&arrsorc[3], "Gishyan", 7);
@@ -20,7 +20,7 @@ void Test_CSArrayString::IndexOperators()
 
     strs0.set_data(&strs0, arrsorc, 4, 8);
     /*Check 0*/
-    if (c_string_compare_chararray(strs0.at(&strs0, 0), "", 0))
+    if (c_string_compare_chararray(strs0.at(&strs0, 0), "", 1))
     {
 	if (result)
 	{
@@ -28,8 +28,8 @@ void Test_CSArrayString::IndexOperators()
 	    result = false;
 	}
 	std::cout << "\tCheck 0\t" << std::endl;
-	std::cout << "\tc_string_compare_chararray(strs0.at(&strs0, 0), "", 0)     = " 
-	    << c_string_compare_chararray(strs0.at(&strs0, 0), "", 0) << "\t:: must be 0" << std::endl;
+	std::cout << "\tc_string_compare_chararray(strs0.at(&strs0, 0), "", 1)     = " 
+	    << c_string_compare_chararray(strs0.at(&strs0, 0), "", 1) << "\t:: must be 0" << std::endl;
     }
 
     /*Check 1*/
