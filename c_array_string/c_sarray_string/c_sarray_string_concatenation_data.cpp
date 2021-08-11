57048f40635746035aba11095b94c634d504eff8
Vahagn Gishyan <vahagn.gishyan.a@gmail.com>
2021-08-11 15:00:31 +0400

    Concatenation data

--- c_array_string/c_sarray_string/c_sarray_string_concatenation_data.cpp
+++ c_array_string/c_sarray_string/c_sarray_string_concatenation_data.cpp
@@ -8,7 +8,7 @@ void c_sarray_string::concatenation_string_array(t_c_sarray_string* obj, t_c_str
 	return;
     index = -1;
     while (++index < size)
-	*obj->at(obj, index + obj->m_size) = arr[index];
+	c_string_copy_c_string(obj->at(obj, index + obj->m_size), arr + index);
     obj->m_size += size;
 }
 void c_sarray_string::concatenation_c_sarray_string(t_c_sarray_string* obj, t_c_sarray_string* arrint)
@@ -19,6 +19,6 @@ void c_sarray_string::concatenation_c_sarray_string(t_c_sarray_string* obj, t_c_
 	return;
     index = -1;
     while (++index < arrint->size(arrint))
-	*obj->at(obj, index + obj->m_size) = *arrint->at(arrint, index);
+	c_string_copy_c_string(obj->at(obj, index + obj->m_size), arrint->at(arrint, index));
     obj->m_size += arrint->m_size;
 }
