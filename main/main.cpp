381b53c5af3aec365237fab68cdab94655dbf11d
Vahagn Gishyan <vahagn.gishyan.a@gmail.com>
2021-08-12 11:21:02 +0400

    Main

--- main/main.cpp
+++ main/main.cpp
@@ -8,17 +8,23 @@ int main()
     std::cout << "Start main()\n";  //for screen heap
 
     std::cout << ""; //for screen heap
-    Test_CSString::All();
-    Test_CDString::All();
-    Test_CString::All();
-    Test_CSArrayInt::All();
-    Test_CDArrayInt::All();
-    Test_CVectorInt::All();
-    Test_CSArrayString::All();
+    //Test_CSString::All();
+    //Test_CDString::All();
+    //Test_CString::All();
+    //Test_CSArrayInt::All();
+    //Test_CDArrayInt::All();
+    //Test_CVectorInt::All();
+    //Test_CSArrayString::All();
+    //Test_CDArrayString::All();
+    //Test_CVectorString::All();
     std::cout << ""; //for screen heap
-
-    _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
 
     std::cout << "Exit main()\n";  //for screen heap
     return 0;
 }
+
+/*
+
+TODO: c_array_string => c_vector_string
+
+*/
