a10c2c0952b757afc86a397feef8c0698f821014
Vahagn Gishyan <vahagn.gishyan.a@gmail.com>
2021-08-13 20:38:59 +0400

    Support functions.h

--- c_is_easy/supporting_functions/supporting_functions.h
+++ c_is_easy/supporting_functions/supporting_functions.h
@@ -18,8 +18,14 @@ void int_array_copy_int_array(int* arr_main, int* arr_other, int size);
 
 
 /*c string array*/
+/*c string array constructor.c*/
 void c_string_array_constructor(t_c_string* strs, int size);
+/*c string array destructor.c*/
 void c_string_array_destructor(t_c_string* strs, int size);
 
+/*c string array set data.c*/
 void c_string_array_set_char_array(t_c_string* strs, int strs_size, const char* arr, int arr_size);
 void c_string_array_set_c_string(t_c_string* strs, int strs_size, const t_c_string *str);
+
+/*c string array copy.c*/
+void c_string_array_copy_c_string_array(t_c_string* str_main, t_c_string* str_other, int size);
