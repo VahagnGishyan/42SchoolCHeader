ecf940ed75f781415640959ae615e96da41aef36
Vahagn Gishyan <vahagn.gishyan.a@gmail.com>
2021-08-19 09:48:39 +0400

    Update

---
+++ c_array_string/c_darray_string/c_darray_string_resize_clear.c
@@ -0,0 +1,89 @@
+#include "c_darray_string.h"
+
+bool c_darray_string_resize(t_c_darray_string* obj, int length)
+{
+    t_c_string*	newarr;
+    int		newlength;
+    int		size;
+
+    if (length < 0)
+    {
+	c_darray_string_clear(obj);
+	return (false);
+    }
+    if (c_darray_string_capacity(obj) >= length)
+	return true;
+
+    newlength = length * 1.3;
+    if ((newarr = (t_c_string*)malloc(newlength * sizeof(t_c_string))) == NULL)
+	return NULL;
+
+    c_string_array_constructor(newarr, newlength);
+    c_string_array_copy_c_string_array(newarr, obj->m_data, obj->m_size);
+    size = obj->m_size;
+    c_darray_string_clear(obj);
+
+    obj->m_data = newarr;
+    obj->m_size = size;
+    obj->m_capacity = newlength;
+    return true;
+}
+
+void c_darray_string_clear(t_c_darray_string* obj)
+{
+    int index;
+
+    if (obj->m_data == NULL)
+	return;
+    index = -1;
+    while (++index < obj->m_capacity)
+    {
+	 c_string_clear(&obj->m_data[index]);
+    }
+    c_string_array_destructor(obj->m_data, obj->m_size);
+
+    free(obj->m_data);
+    obj->m_data = NULL;
+    obj->m_size = 0;
+    obj->m_capacity = 0;
+}
+
+
+
+//void c_darray_string_disconnect(t_c_darray_string* obj)
+//{
+//    if (m_data == NULL)
+//	return;
+//
+//    c_darray_string_clear(obj);
+//    obj->m_data = NULL;
+//    obj->m_capacity = 0;
+//}
+
+//bool c_darray_string_reserve(t_c_darray_string* obj, int length)
+//{
+//    return (length >= 0 && length <= obj->m_capacity);
+//}
+//
+//bool c_darray_string_resize(t_c_darray_string* obj, int length)
+//{
+//    return (length >= 0 && length <= obj->m_capacity);
+//}
+//
+//void c_darray_string_clear(t_c_darray_string* obj)
+//{
+//    if (m_data == NULL)
+//	return;
+//    c_string_array_destructor(m_data, obj->m_size);
+//    obj->m_size = 0;
+//}
+//
+//void c_darray_string_disconnect(t_c_darray_string* obj)
+//{
+//    if (m_data == NULL)
+//	return;
+//
+//    c_darray_string_clear(obj);
+//    obj->m_data = NULL;
+//    obj->m_capacity = 0;
+//}
